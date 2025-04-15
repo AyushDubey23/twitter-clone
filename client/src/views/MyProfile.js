5b3b70485bc1c80f8bdab295f394d51e78b62393
AyushDubey23 <ayushdubey3740@gmail.com>
2025-04-16 01:17:15 +0530

    Add files via upload

---
+++ client/src/views/MyProfile.js
@@ -0,0 +1,18 @@
+// MyProfile.js
+import React from "react";
+
+const MyProfile = () => {
+  return (
+    <div>
+      <h1>Welcome to Ayush's Profile!</h1>
+      <p>Here's a little about me:</p>
+      <ul>
+        <li>I'm a CSE student at MMMUT.</li>
+        <li>I love coding and exploring new technologies!</li>
+        <li>Connect with me on LinkedIn: <a href="https://www.linkedin.com/in/ayush-dubey-508196331/">Ayush's LinkedIn</a></li>
+      </ul>
+    </div>
+  );
+};
+
+export default MyProfile;
